a36f508f28e7f27bc40acd1b8365ed315d074c2d
Oleksandr Vlasov <drumlife182@gmail.com>
2024-07-07 00:22:59 +0200

    Finished working with react elements

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('react');
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const el = (
+  <div>
+    <h1>My Todo List</h1>
+    <input  placeholder="search"/>
+    <ul>
+      <li>React App</li>
+      <li>Build Awesome Appliaction</li>
+    </ul>
+  </div>
+  )
+console.log(el);
+const container = document.getElementById('root')
+const root = createRoot(container);
+
+root.render(el);
