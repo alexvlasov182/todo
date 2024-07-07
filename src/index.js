2dfa2da6e15a0a0482933a21ad308f4145fe37a4
Oleksandr Vlasov <drumlife182@gmail.com>
2024-07-07 13:35:42 +0200

    jsx

--- src/index.js
+++ src/index.js
@@ -2,33 +2,36 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 
 const TodoList = () => {
+  const items = ["Learn React", "Build Awesome App"];
+
   return (
     <ul>
-      <li>React App</li>
-      <li>Build Awesome Appliaction</li>
+      <li>{items[0]}</li>
+      <li>{items[1]}</li>
     </ul>
-  )
+  );
 };
 
 const AppHeader = () => {
-  return <h1>My Todo List</h1>
-  
+  return <h1>My Todo List</h1>;
 };
 
 const SearchPanel = () => {
-  return <input  placeholder="search"/>
+  const searchText = "Type here to search";
+  const searchStyle = { fontSize: "20px" };
+  return <input style={searchStyle} placeholder={searchText} />;
 };
 
 const App = () => {
-  return  (
+  return (
     <div>
-    <AppHeader />
-    <SearchPanel />
-    <TodoList />
-  </div>
-  )
+      <AppHeader />
+      <SearchPanel />
+      <TodoList />
+    </div>
+  );
 };
 
-const container = document.getElementById('root')
+const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />);
