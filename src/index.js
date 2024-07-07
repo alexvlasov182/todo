ab543a1d7a6cb6bfab68616a960618e7bc6243ba
Oleksandr Vlasov <drumlife182@gmail.com>
2024-07-07 11:51:02 +0200

    Finished working with React components

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-const el = (
-  <div>
-    <h1>My Todo List</h1>
-    <input  placeholder="search"/>
+const TodoList = () => {
+  return (
     <ul>
       <li>React App</li>
       <li>Build Awesome Appliaction</li>
     </ul>
+  )
+};
+
+const AppHeader = () => {
+  return <h1>My Todo List</h1>
+  
+};
+
+const SearchPanel = () => {
+  return <input  placeholder="search"/>
+};
+
+const App = () => {
+  return  (
+    <div>
+    <AppHeader />
+    <SearchPanel />
+    <TodoList />
   </div>
   )
-console.log(el);
+};
+
 const container = document.getElementById('root')
 const root = createRoot(container);
-
-root.render(el);
+root.render(<App />);
